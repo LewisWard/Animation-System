5f5804e91ac82ed22da31375f4d6b2459fcaac9d
Lewis Ward <lewis.ward@outlook.com>
2015-04-14 17:46:53 +0100

    Shaders now compute normals and apply to the final output, this makes it a little easier to see the mesh. Updated meshes.

--- AnimationSystem/AnimationSystem/shaders/joint.pix.glsl
+++ AnimationSystem/AnimationSystem/shaders/joint.pix.glsl
@@ -1,9 +1,25 @@
+// Author  : Lewis Ward
+// Program : Animation System Tech Demo
+// Date    : 04/04/2015
+
 #version 330 ///< shader version
 
 out vec4 output_colour; ///< output colour
 
+in vec3 ps_light; // light direction
+in vec3 ps_eye; // eye direction
+in vec3 ps_normal; // normal vector
+
 void main()
 {
+  // make sure the vectors are normalized
+  vec3 L = normalize(ps_light);
+  vec3 E = normalize(ps_eye);
+  vec3 N = normalize(ps_normal);
+
+  // compute the dot product between the normal and light
+  float dotNL = max(dot(N, L), 0.0);
+
   // output colour
-  output_colour = vec4(0.0, 1.0, 1.0, 1.0);
+  output_colour = vec4(0.0, 1.0, 1.0, 1.0) * dotNL;
 }
--- AnimationSystem/AnimationSystem/shaders/joint.vtx.glsl
+++ AnimationSystem/AnimationSystem/shaders/joint.vtx.glsl
@@ -1,11 +1,30 @@
+// Author  : Lewis Ward
+// Program : Animation System Tech Demo
+// Date    : 04/04/2015
+
 #version 330 ///< shader version
 layout(location = 0) in vec4 position; ///< the vertex co-odinate from VBO
-layout(location = 1) in vec4 normal; ///< the UV co-odinate from VBO
+layout(location = 1) in vec3 normal; ///< the UV co-odinate from VBO
 
 uniform mat4 mvp; ///< Model-View-Projection
+uniform mat4 mv;  ///< Model-View
+
+out vec3 ps_light; // light direction
+out vec3 ps_eye; // eye direction (for the moment will be the same as the light!)
+out vec3 ps_normal; // normal vector
 
 void main()
 {
+	// transform the normal into viewspace
+	ps_normal = (mv * vec4(normal, 0.0)).xyz;
+
+	// compute vector from light to position
+	vec4 L = vec4(0, 0, 0, 0) - mv * normalize(position);
+
+	// pass to fragment shader
+	ps_light = L.xyz;
+	ps_eye = L.xyz; ///< same as light for the moment!
+
 	// position output
 	gl_Position =  mvp * position;
 }
